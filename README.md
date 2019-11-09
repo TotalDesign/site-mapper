6970b5c718867523be30dc2283bf90a7061d8825
Robbert van 't Westende <robbert@totaldesign.com>
2019-11-09 21:10:52 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Site Indexer
+
+This tool helps with indexing websites for further processing.
+
+## Setup
+
+    npm i
+
+## Running
+
+    npm start [url] [output file]
---
+++ index.js
@@ -0,0 +1,123 @@
+const args = process.argv.slice(2);
+
+const cheerio = require('cheerio');
+const csv = require('csv');
+const fs = require('fs');
+const Crawler = require('simplecrawler');
+const Spinner = require('cli-spinner').Spinner;
+
+const spinner = new Spinner({
+  text: 'Crawling... %s'
+});
+
+spinner.setSpinnerString('|/-\\');
+spinner.start();
+
+const incrementAmountResourcesCrawled = () => {
+  amountResourcesCrawled++;
+  spinner.setSpinnerTitle(`Crawling... %s => ${amountResourcesCrawled}`);
+};
+
+const writeStream = fs.createWriteStream(args[1]);
+
+/**
+ * Create CSV stringify stream
+ * @type {stringify.Stringifier}
+ */
+const stringifier = csv.stringify({
+  columns: ['url', 'status', 'title', 'description', 'contentType', 'location'],
+  header: true,
+});
+
+/**
+ * Write to file streaming
+ */
+stringifier.on('readable', () => {
+  let row;
+  while((row = stringifier.read())) {
+    writeStream.write(row);
+  }
+});
+
+/**
+ * Create Crawler
+ * @type {Crawler}
+ */
+const crawler = new Crawler(args[0]);
+let amountResourcesCrawled = 0;
+
+crawler.maxDepth = 5;
+crawler.maxConcurrency = 10;
+crawler.parseHTMLComments = false;
+crawler.parseScriptTags = false;
+crawler.respectRobotsTxt = false;
+
+const handleNonHttpOkStatus = (queueItem, redirectQueueItem, response) => {
+  const record = {};
+
+  record.url = queueItem.url;
+  record.status = queueItem.stateData.code;
+
+  if (response && response.headers && response.headers['location']) {
+    record.location = response.headers['location'];
+  }
+
+  stringifier.write(record);
+
+  incrementAmountResourcesCrawled();
+};
+
+/**
+ * Extract properties from page when crawled
+ */
+crawler.on('fetchcomplete', (queueItem, responseBuffer, response) => {
+  const record = {};
+
+  record.url = queueItem.url;
+  record.status = queueItem.stateData.code;
+  record.contentType = response.headers['content-type'];
+
+  if (0 === response.headers['content-type'].indexOf('text/html')) {
+    const page = cheerio.load(responseBuffer.toString('utf8'));
+
+    record.title = page('title').text();
+    record.description = page('meta[name="description"]').attr('content');
+  }
+
+  stringifier.write(record);
+
+  incrementAmountResourcesCrawled();
+});
+
+crawler.on('fetchredirect', handleNonHttpOkStatus);
+crawler.on('fetch404', handleNonHttpOkStatus);
+crawler.on('fetch410', handleNonHttpOkStatus);
+crawler.on('fetcherror', handleNonHttpOkStatus);
+
+/**
+ * When the crawler completes
+ */
+crawler.on('complete', () => {
+  stringifier.end();
+  spinner.stop();
+});
+
+/**
+ * Override resource discovery method to only follow a[href] links in pages in order to only crawl relevant resources
+ *
+ * @param buffer
+ * @param queueItem
+ * @returns {*}
+ */
+crawler.discoverResources = (buffer, queueItem) => {
+  const page = cheerio.load(buffer.toString('utf8'));
+
+  return page('a[href]').map(function () {
+    return page(this).attr('href');
+  }).get();
+};
+
+/**
+ * Start crawler
+ */
+crawler.start();
